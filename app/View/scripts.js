4337d1fdd0eb801f60fe4524a9af5930863891da
Joel Holsteen <joel.holsteen@usmac1928jhols.schq.secious.com>
2015-12-11 12:21:06 -0700

    animationgit add View/index.html View/scripts.js git add View/index.html View/scripts.js

--- app/View/scripts.js
+++ app/View/scripts.js
@@ -26,7 +26,9 @@ var sigCanvas = new sigma(
 			defaultEdgeColor: "rgb(175,175,175)",
 			edgeColor: "default",
 			minEdgeSize: 1,
-			maxEdgeSize: 2
+			maxEdgeSize: 2,
+			labelThreshold: 10,
+			animationsTime: 1000
 		},
 	});
 
@@ -63,6 +65,33 @@ sigCanvas.bind('overNode', function(e) {
 });
 
 
+// sigCanvas.bind('clickStage', function(e){
+// 	var nodes = sigCanvas.graph.nodes()
+// 	// nodes.forEach(funciton(n){
+// 		sigma.plugins.animate(sigCanvas sigma instance,
+// 			{
+// 			    //property names to be animated to
+// 			    x: 'circular_' + 'x',
+//       			y: 'circular_' + 'y',
+//       		});
+// 	// });
+	
+// })
+// var step = 0;
+
+// setInterval(function() {
+//   // var prefix = ['grid_', 'circular_'][step = +!step];
+//   sigma.plugins.animate(
+//     sigCanvas,
+//     {
+//       x: "condensedX",
+//       y: "condensedY",
+//     }
+//   );
+// }, 2000);
+
+
+
 sigCanvas.bind('outNode', function(e) {
     sigCanvas.graph.nodes().forEach(function(n) {
     	n.label = n.originalLabel
@@ -81,6 +110,18 @@ sigCanvas.bind('outNode', function(e) {
 
 setInterval(timer, 1000);
 function timer() {
+	// if(needsUpdate == "1" || firstTime)
+	// {
+	// 	sigma.plugins.animate(
+	// 		    sigCanvas,
+	// 		    {
+	// 		      x: "condensedX",
+	// 		      y: "condensedY",
+	// 		    }
+	// 	  	);
+	// }
+	
+
 	var needsUpdate = "0";
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
@@ -94,14 +135,45 @@ function timer() {
 
 	if(needsUpdate == "1" || firstTime)
 	{
-		console.log("DRAWING");
-		sigma.parsers.json(
+		if(!firstTime)
+		{
+			console.log("HERE");
+			sigma.plugins.animate(
+			    sigCanvas,
+			    {
+			      x: "condensedX",
+			      y: "condensedY",
+			    },
+			    {
+		    		onComplete: function(){
+				    		doUpdate();
+				    	}
+			    }
+			    
+		  	);
+			    
+		}
+		else
+		{
+			doUpdate();
+		}
+
+
+		needsUpdate = "";
+		firstTime = false;
+	}
+	
+}
+
+
+var doUpdate = function()
+{
+	sigma.parsers.json(
 		  'data.json',
 		  sigCanvas,
 		  function() {
-		  	console.log(sigCanvas.graph.nodes())
+
 			sigCanvas.graph.nodes().forEach(function(n) {
-				console.log(n);
 				if(n.label[0] == "i")
 				{
 					n.color = "#00BFFF"
@@ -116,6 +188,13 @@ function timer() {
 				}
 				n.originalLabel = n.label;
 				n.originalColor = n.color;
+				n.condensedX = 0;
+				n.condensedY = 0;
+				n.originalX = n.x;
+				n.originalY = n.y;
+				n.x = n.condensedX;
+				n.y = n.condensedY;
+
 			});
 			sigCanvas.graph.edges().forEach(function(e) {
 				e.originalSize = e.size;
@@ -124,15 +203,75 @@ function timer() {
 
 			});
 		    sigCanvas.refresh();
-
+		    if(sigCanvas.graph.nodes().length > 0)
+			{
+				sigma.plugins.animate(
+				    sigCanvas,
+				    {
+				      x: "originalX",
+				      y: "originalY",
+				    }
+		  		);
+			}
 		  }
 		);
-
-
-
-		needsUpdate = "";
-		firstTime = false;
-	}
-	
 }
 
+// var i,
+//     s,
+//     o,
+//     L = 10,
+//     N = 100,
+//     E = 500,
+//     g = {
+//       nodes: [],
+//       edges: []
+//     },
+//     step = 0;
+// // Generate a random graph:
+// for (i = 0; i < N; i++) {
+//   o = {
+//     id: 'n' + i,
+//     label: 'Node ' + i,
+//     circular_x: L * Math.cos(Math.PI * 2 * i / N - Math.PI / 2),
+//     circular_y: L * Math.sin(Math.PI * 2 * i / N - Math.PI / 2),
+//     circular_size: Math.random(),
+//     circular_color: '#' + (
+//       Math.floor(Math.random() * 16777215).toString(16) + '000000'
+//     ).substr(0, 6),
+//     grid_x: i % L,
+//     grid_y: Math.floor(i / L),
+//     grid_size: 1,
+//     grid_color: '#ccc'
+//   };
+//   ['x', 'y', 'size', 'color'].forEach(function(val) {
+//     o[val] = o['grid_' + val];
+//   });
+//   g.nodes.push(o);
+// }
+// for (i = 0; i < E; i++)
+//   g.edges.push({
+//     id: 'e' + i,
+//     source: 'n' + (Math.random() * N | 0),
+//     target: 'n' + (Math.random() * N | 0)
+//   });
+// // Instantiate sigma:
+// s = new sigma({
+//   graph: g,
+//   container: 'graph-Contain',
+//   settings: {
+//     animationsTime: 1000
+//   }
+// });
+// setInterval(function() {
+//   var prefix = ['grid_', 'circular_'][step = +!step];
+//   sigma.plugins.animate(
+//     s,
+//     {
+//       x: prefix + 'x',
+//       y: prefix + 'y',
+//       size: prefix + 'size',
+//       color: prefix + 'color'
+//     }
+//   );
+// }, 2000);
